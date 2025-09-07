37f11d212db447c29158d8d009fc9844bbbaa8fb
Shmouel kessous <chmouel2690@gmail.com>
2025-09-07 15:12:39 +0300

    save

--- src/app/drawing-layer.component.ts
+++ src/app/drawing-layer.component.ts
@@ -1,17 +1,17 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { input } from '@angular/core';
 import { DrawingObject } from './drawing.service';
-import { DrawingComponent } from './drawing-component';
+import { DrawingElementComponent } from './drawing/drawing-element.component';
 
 @Component({
   selector: 'app-drawing-layer',
-  imports: [DrawingComponent],
+  imports: [DrawingElementComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     @if (objects().length > 0) {
       <div style="position: absolute; left: 0; top: 0; width: 100%; height: 100%; pointer-events: none;">
         @for (obj of objects(); track obj) {
-          <app-drawing-component [bbox]="obj"></app-drawing-component>
+          <app-drawing-element [bbox]="obj"></app-drawing-element>
         }
       </div>
     }
