9287807ac22ea1ba3c97fca8aa7bc84e5e380c41
Shmouel kessous <chmouel2690@gmail.com>
2025-09-07 14:44:37 +0300

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NgxExtendedPdfViewerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ng-redactor';
+  pdfSrc = 'assets/test.pdf';
 }
